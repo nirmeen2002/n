434f0206b10299b2450ea5c4719445f2e73e3db4
nirmeen2002 <104161508+nirmeen2002@users.noreply.github.com>
2023-12-30 01:25:17 +0200

    team.c

--- team.c
+++ team.c
@@ -1,14 +1,38 @@
 // team.c
 
 #include "supermarket.h"
+#include <stdio.h>
+#include <unistd.h>
 
-// Function definitions for team manager and employee threads
 void* shelfEmployee(void* args) {
-    // Employee logic here
+    Team* team = (Team*)args;
+    Supermarket* supermarket = team->supermarket;
+
+    while (1) {
+        // Employee shelving logic (to be expanded upon)
+        sleep(1); // Simulate work
+
+        // Check for termination condition (to be implemented)
+    }
     return NULL;
 }
 
 void* teamManager(void* args) {
-    // Manager logic here
+    Team* team = (Team*)args;
+    Supermarket* supermarket = team->supermarket;
+
+    while (1) {
+        for (int i = 0; i < supermarket->productCount; i++) {
+            pthread_mutex_lock(&supermarket->storageMutex);
+            if (supermarket->products[i].shelfAmount < REPLENISH_THRESHOLD) {
+                supermarket->products[i].shelfAmount = supermarket->shelfCapacity;
+                printf("Manager of Team %d replenished Product %d (Shelf Amount: %d)\n", team->teamId, i, supermarket->products[i].shelfAmount);
+            }
+            pthread_mutex_unlock(&supermarket->storageMutex);
+        }
+        sleep(5);  // Simulate management tasks
+
+        // Check for termination condition (to be implemented)
+    }
     return NULL;
 }
