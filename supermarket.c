055f96da77df61803b41534975b5ab47ffc94299
nirmeen2002 <104161508+nirmeen2002@users.noreply.github.com>
2023-12-30 01:07:17 +0200

    supermarket.c

---
+++ supermarket.c
@@ -0,0 +1,14 @@
+// supermarket.c
+
+#include "supermarket.h"
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+
+void initializeSupermarket(Supermarket* supermarket) {
+    // Initialize supermarket data
+}
+
+void cleanupSupermarket(Supermarket* supermarket) {
+    // Free resources and destroy mutexes
+}
