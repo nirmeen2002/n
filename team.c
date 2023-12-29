e72dd4cdafe3c84c25199e0b908c73f92c2b63a1
nirmeen2002 <104161508+nirmeen2002@users.noreply.github.com>
2023-12-30 01:07:50 +0200

    team.c

---
+++ team.c
@@ -0,0 +1,14 @@
+// team.c
+
+#include "supermarket.h"
+
+// Function definitions for team manager and employee threads
+void* shelfEmployee(void* args) {
+    // Employee logic here
+    return NULL;
+}
+
+void* teamManager(void* args) {
+    // Manager logic here
+    return NULL;
+}
