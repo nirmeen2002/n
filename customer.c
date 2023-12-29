f2db33aebec48fdbc0b3476a9a9199fbbb97dfd4
nirmeen2002 <104161508+nirmeen2002@users.noreply.github.com>
2023-12-30 01:08:46 +0200

    customer.c

---
+++ customer.c
@@ -0,0 +1,10 @@
+// customer.c
+
+#include "supermarket.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void customerProcess(Customer* customer, Supermarket* supermarket) {
+    // Customer logic here
+}
