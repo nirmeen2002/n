ec443e757d6b847bc52a69f53655be470d86a5de
nirmeen2002 <104161508+nirmeen2002@users.noreply.github.com>
2023-12-30 21:47:52 +0200

    customer.c

--- customer.c
+++ customer.c
@@ -4,24 +4,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <time.h>
 
 void customerProcess(Customer* customer, Supermarket* supermarket) {
-    srand(time(NULL) ^ (getpid()<<16));
+    srand(time(NULL) ^ (getpid() << 16)); // Ensure different seed for each process
+
     while (1) {
+        // Customer randomly chooses a product
         int productId = rand() % supermarket->productCount;
-        int quantity = rand() % 5 + 1;  // Random quantity from 1 to 5
+        // Customer randomly decides on a quantity
+        int quantity = rand() % 5 + 1; // Random quantity between 1 and 5
 
         pthread_mutex_lock(&supermarket->storageMutex);
         if (supermarket->products[productId].shelfAmount >= quantity) {
+            // If enough stock is available, customer buys the product
             supermarket->products[productId].shelfAmount -= quantity;
             printf("Customer %d bought %d of Product %d (Shelf Amount: %d)\n", customer->customerId, quantity, productId, supermarket->products[productId].shelfAmount);
         } else {
-            printf("Customer %d couldn't buy Product %d (Insufficient stock)\n", customer->customerId, productId);
+            printf("Customer %d could not buy Product %d (Insufficient stock)\n", customer->customerId, productId);
         }
         pthread_mutex_unlock(&supermarket->storageMutex);
 
-        sleep(rand() % 5 + 1);  // Simulate shopping time
+        // Simulate time taken for shopping before choosing another product
+        sleep(rand() % 5 + 1); // Sleep for 1 to 5 seconds
 
-        // Check for termination condition (to be implemented)
+        // Implement termination condition check if needed
     }
 }
